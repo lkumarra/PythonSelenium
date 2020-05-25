56c4564d766fad6ee5965499d0ea49c275a9eb3a
lkumarra <Lavendra.rajputc1@gmail.com>
2020-05-25 18:31:23 +0530

    Added xml plugin

--- PageObjectModel/test_case/test_home_page.py
+++ PageObjectModel/test_case/test_home_page.py
@@ -8,7 +8,7 @@ from actions.login_page import LoginPage
 from test_base.page import Page
 import pytest
 
-
+@pytest.mark.usefixtures("setUp")
 class TestHomePage():
     
     fileName = "../test_data/Data.json"
@@ -27,12 +27,12 @@ class TestHomePage():
         yield
         self.page.quit_session(self.driver)
 
-    def test_verifyWelcomeMessage(self, setUp):
+    def test_verifyWelcomeMessage(self):
         actual = self.homePage.verify_welcome_message()
         expected = self.data["HomePage"]["welcomeMessage"]
         assert actual == expected, "Wrong Manager Id is displayed"
 
-    def test_verifyManagerId(self, setUp):
+    def test_verifyManagerId(self):
         actual = self.homePage.verify_manager_id()
         expected = self.data["HomePage"]["customerId"]
         assert actual == expected, "Wrong welcome message is displayed"
