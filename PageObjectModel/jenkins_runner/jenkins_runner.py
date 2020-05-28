45a123c0f59b4452c5a7a1ff78d80a3421c68eed
lkumarra <Lavendra.rajputc1@gmail.com>
2020-05-28 16:35:45 +0530

    Removed Rough Package

---
+++ PageObjectModel/jenkins_runner/jenkins_runner.py
@@ -0,0 +1,25 @@
+"""
+Created on 28-May-2020
+@author: Lavendra rajput
+"""
+
+from jenkins_utils.jenkins_utils import DevOpsJenkins
+import tkinter as tk 
+
+def on_start():
+    NAME_OF_JOB = "Python_Automation_Framework"
+    TOKEN_NAME = "python_automation_framework"
+    # Example Parameter
+    PARAMETERS = None
+    jenkins_obj = DevOpsJenkins()
+    output = jenkins_obj.build_job(NAME_OF_JOB, PARAMETERS, TOKEN_NAME)
+    label1 = tk.Label(root, text= "Jenkins Build URL: {}".format(output['url']))
+    label1.pack()
+    print ("Jenkins Build URL: {}".format(output['url']))
+
+root = tk.Tk()
+root.geometry("800x200")
+root.title("Jenkins Buid Trigger")
+button1 = tk.Button(root, text = "Start",bg = 'green', command =on_start, height = 2, width = 10)
+button1.pack()
+root.mainloop()
--- PageObjectModel/rough/rough.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 27-Apr-2020
-
-@author: Lavendra rajput
-'''
-import subprocess
-
-subprocess.run("cd test_case", shell = True)
